1619a84cbc8bf9cd7940792b4da7870e337e7d11
crespo21 <59964121+crespo21@users.noreply.github.com>
2020-02-25 08:42:15 +0200

    Update FrameSomeText.java

--- src/main/java/FrameSomeText.java
+++ src/main/java/FrameSomeText.java
@@ -2,31 +2,40 @@ class  FrameSomeText {
 public static void frame(String[] check ){
         int longestStringLength = 0;
         for (int i=0; i < check.length; i++) {
-        if (check[i].length() > longestStringLength){
+           if (check[i].length() > longestStringLength){
+                longestStringLength = check[i].length();   //
+           }
 
-          longestStringLength = check[i].length();   //
         }
-
-        }
+        
         for (int i= 0; i < longestStringLength+4;i++){
-        System.out.print("*"); //top
+                System.out.print("*"); //top
         }
 
         System.out.println(" ");
-        for (int i= 0; i < check.length;i++){ // middle
-        System.out.print("* " + check[i]);
+        
+        // middle
+        
+        for (int i= 0; i < check.length;i++){ 
+                System.out.print("* " + check[i]);
+                
+                int NoOfSpaces = longestStringLength - check[i].length()+1;
+                
+            for (int j =0; j <NoOfSpaces;j++ ){
+                    
+                 System.out.print(" " );
+            }
+                 System.out.println("*");
 
-        int NoOfSpaces = longestStringLength - check[i].length()+1;
-        for (int j =0; j <NoOfSpaces;j++ ){
-                System.out.print(" " );
         }
-                System.out.println("*");
-
-        }
+        
+        //bottom
+        
         for (int i= 0; i < longestStringLength+4;i++){
-        System.out.print("*"); //bottom
+                
+                System.out.print("*"); 
         }
-        }
+}
 public static void main(String[] args) {
         String [] check = {"Write", "good", "code ", "every", "day","and","make","sure","it's","flexible"};
         frame(check);
