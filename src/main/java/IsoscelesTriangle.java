c8b7ea0b9917d84184916617be090839c2dd6eab
crespo21 <59964121+crespo21@users.noreply.github.com>
2020-02-25 08:44:06 +0200

    Update IsoscelesTriangle.java

--- src/main/java/IsoscelesTriangle.java
+++ src/main/java/IsoscelesTriangle.java
@@ -1,33 +1,20 @@
 import java.util.Scanner;
-
 public class IsoscelesTriangle {
-
-
     public static void isosceles(int index){
-
-
         for (int i = 0; i <=index; i++){
-
-
             for (int j = 2; j>=i; j--){
                 System.out.print(" ");
-
             }
             for (int k = 1; k<=(2*i); k++){
-
                 System.out.print("*");
             }
-
             System.out.println("*");
         }
-
     }
-
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter any number greater than 0 to build an Isosceles Triangle: ");
         int index = input.nextInt();
         isosceles(index);
     }
-
 }
